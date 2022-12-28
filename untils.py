2b87ade2a7affab05ee014c7bf342c10977dafd5
jiangsijia9495 <13357725372@163.com>
2022-12-27 19:22:59 -0500

    Update untils.py

--- untils.py
+++ untils.py
@@ -1,7 +1,15 @@
 import trimesh
 pointcloud = trimesh.points.PointCloud(render_out_fix['pts'].view(-1,3))
 pointcloud.export('demo.ply')
+
 from pytorch3d.structures import Pointclouds
 pointclouds = Pointclouds(points=[torch.cat([render_out_fix['pts'].view(-1,3),render_out['your_pts']],dim=0)])
 from pytorch3d.io import IO
 IO().save_pointcloud(pointclouds, "output_pointcloud.ply")
+
+f = plt.figure()
+f.add_subplot(1,2, 1)
+plt.imshow(img1)
+f.add_subplot(1,2, 2)
+plt.imshow(img2)
+plt.show(block=True)
